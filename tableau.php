5b07faece5873cb709457db2b75e3e69d3d4e827
Wassila-Baz <wassila.baz@icloud.com>
2024-06-18 15:41:36 +0200

    Initial commit

---
+++ tableau.php
@@ -0,0 +1,97 @@
+<?php
+$oeuvres = [
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ],
+    [
+        "titre" => '',
+        "image" => '',
+        "description" => '',
+        "description_complete" => ''
+    ]
+];
+
+// Exemple d'affichage des titres
+
+?>
